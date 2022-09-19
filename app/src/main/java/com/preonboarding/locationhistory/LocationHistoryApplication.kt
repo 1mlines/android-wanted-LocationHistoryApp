9c1618675ed9b6fce160b298990e7139c7c2e4fc
gyurim <gyurim1250@gmail.com>
2022-09-19 22:24:10 +0900

    feat: 기본 애플리케이션 클래스 추가

---
+++ app/src/main/java/com/preonboarding/locationhistory/LocationHistoryApplication.kt
@@ -0,0 +1,9 @@
+package com.preonboarding.locationhistory
+
+import android.app.Application
+
+class LocationHistoryApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
