4b6ec5362dccf7a59aa7e90b0b5df29e1673fad9
Hyunkuk <boris0815@naver.com>
2022-09-19 19:51:37 +0900

    [rename] move main activity to presentaion/ui/main directory

--- app/src/main/java/com/preonboarding/locationhistory/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/preonboarding/locationhistory/presentation/ui/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.preonboarding.locationhistory
+package com.preonboarding.locationhistory.presentation.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.preonboarding.locationhistory.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
