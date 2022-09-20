28a8d15c1ce36bd2b2b75b56df881ca9b702ddca
seoyoon513 <110798031+seoyoon513@users.noreply.github.com>
2022-09-20 18:53:13 +0900

    [feat] : mapview 띄우기

--- app/src/main/java/com/preonboarding/locationhistory/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/preonboarding/locationhistory/presentation/ui/main/MainActivity.kt
@@ -1,14 +1,46 @@
 package com.preonboarding.locationhistory.presentation.ui.main
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.preonboarding.locationhistory.R
 import dagger.hilt.android.AndroidEntryPoint
+import net.daum.mf.map.api.MapView
+import timber.log.Timber
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.security.Signature
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapViewContainer = findViewById<ViewGroup>(R.id.mapview_kakaomap)
+        val mapView = MapView(this)
+        mapViewContainer.addView(mapView)
     }
+
+    // key hash값 얻기
+//    private fun getAppKeyHash() {
+//        try {
+//            val info =
+//                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+//            for (signature in info.signatures) {
+//                var md: MessageDigest
+//                md = MessageDigest.getInstance("SHA")
+//                md.update(signature.toByteArray())
+//                val something = String(Base64.encode(md.digest(), 0))
+//                Log.e("Hash key", something)
+//            }
+//        } catch (e: Exception) {
+//
+//            Log.e("name not found", e.toString())
+//        }
+//    }
 }
