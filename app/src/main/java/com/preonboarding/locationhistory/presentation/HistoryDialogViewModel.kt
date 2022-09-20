8629a7dde81e43a072949f226d0342f7157587f1
YellowC-137 <55780312+YellowC-137@users.noreply.github.com>
2022-09-20 23:18:10 +0900

    [FEAT] Room DB,DAO,Item 추가 및 HistoryDialog 구현 및 convention 수정 0920 #5

---
+++ app/src/main/java/com/preonboarding/locationhistory/presentation/HistoryDialogViewModel.kt
@@ -0,0 +1,17 @@
+package com.preonboarding.locationhistory.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.preonboarding.locationhistory.data.History
+import com.preonboarding.locationhistory.data.HistoryDao
+import kotlinx.coroutines.flow.Flow
+import java.util.*
+
+class HistoryDialogViewModel(private val historyDao: HistoryDao) : ViewModel() {
+
+    fun getHistory(date: Date): Flow<List<History>> = historyDao.getHistory(myDate = date)
+
+    fun insertHistory(history: History) = historyDao.insertHistory(myHistory = history)
+
+}
+
