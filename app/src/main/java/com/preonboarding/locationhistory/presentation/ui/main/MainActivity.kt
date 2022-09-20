3796c6ee6c1c72c0eb2b0a283fbd517c331c80bb
sujin-kk <sjoo0002@gmail.com>
2022-09-21 01:43:10 +0900

    [feat] 마커 띄우기 로직 추가

--- app/src/main/java/com/preonboarding/locationhistory/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/preonboarding/locationhistory/presentation/ui/main/MainActivity.kt
@@ -5,6 +5,8 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.preonboarding.locationhistory.R
 import dagger.hilt.android.AndroidEntryPoint
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 
 @AndroidEntryPoint
@@ -35,4 +37,20 @@ class MainActivity : AppCompatActivity() {
 //            Log.e("name not found", e.toString())
 //        }
 //    }
+
+    private fun createMarker(locationName: String, latitude: Double, longitude: Double): MapPOIItem {
+        val marker = MapPOIItem()
+
+        marker.apply {
+            itemName = locationName // 장소 이름
+            mapPoint = MapPoint.mapPointWithGeoCoord(latitude, longitude) // 좌표
+            markerType = MapPOIItem.MarkerType.BluePin  // 기본 블루 마커
+            selectedMarkerType = MapPOIItem.MarkerType.RedPin // 마커 클릭 시 기본 레드 핀
+        }
+        /*
+        마커 띄우기
+        mapView.addPOIItem(marker)
+        */
+        return marker
+    }
 }
