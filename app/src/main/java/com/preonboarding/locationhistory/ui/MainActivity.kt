0e98e98a2cac9ffca973b5cf2a861d118c7341e5
simjunbo <simuelunbo@gmail.com>
2022-09-21 00:43:42 +0900

    feat: 구글 맵 지도 ui 세팅 작업

--- app/src/main/java/com/preonboarding/locationhistory/ui/MainActivity.kt
+++ app/src/main/java/com/preonboarding/locationhistory/ui/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.preonboarding.locationhistory.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.preonboarding.locationhistory.R
+import com.preonboarding.locationhistory.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initBinding()
+        setUpMapView()
+
+    }
+
+    private fun setUpMapView() {
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_map_container) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        val seoul = LatLng(37.56, 126.97)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 10f))
+    }
+
+    private fun initBinding() {
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+        binding.lifecycleOwner = this
     }
 }
