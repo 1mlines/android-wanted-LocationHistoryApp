1683d15de2cc045e721149b5edf4538aeafc1422
DavidKwon7 <khjunejune10@gmail.com>
2022-09-21 00:30:35 +0900

    Add activity_main.xml 버튼 추가

--- app/src/main/java/com/preonboarding/locationhistory/view/MainActivity.kt
+++ app/src/main/java/com/preonboarding/locationhistory/view/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.preonboarding.locationhistory.view
 
+import android.os.Bundle
+import android.widget.Toast
 import com.preonboarding.locationhistory.R
 import com.preonboarding.locationhistory.base.BaseActivity
 import com.preonboarding.locationhistory.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-// test
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.btnAddress.setOnClickListener {
+            Toast.makeText(this, "주소버튼을 눌렀습니다", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
