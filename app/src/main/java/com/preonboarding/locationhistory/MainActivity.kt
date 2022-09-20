5005a97bc3d7b1d77d6d9268322dc0a57104b5a9
DavidKwon7 <khjunejune10@gmail.com>
2022-09-20 22:18:06 +0900

    test

--- app/src/main/java/com/preonboarding/locationhistory/MainActivity.kt
+++ app/src/main/java/com/preonboarding/locationhistory/MainActivity.kt
@@ -4,3 +4,5 @@ import com.preonboarding.locationhistory.base.BaseActivity
 import com.preonboarding.locationhistory.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main)
+
+// test
