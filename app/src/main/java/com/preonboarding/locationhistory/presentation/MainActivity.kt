3e2a282bbd40730c314d85cb1f416bc4da5ca909
yeongjin <dudwls901@naver.com>
2022-09-20 16:08:09 +0900

    [Chore] 라이브러리, 패키지 설정 - 0920

--- app/src/main/java/com/preonboarding/locationhistory/presentation/MainActivity.kt
+++ app/src/main/java/com/preonboarding/locationhistory/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.preonboarding.locationhistory
+package com.preonboarding.locationhistory.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.preonboarding.locationhistory.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
