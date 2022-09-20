19ee29c996f90b2e02779a6d19af9076c51664db
JaesungLeee <viki0114@naver.com>
2022-09-20 22:44:31 +0900

    [FEAT] #1 NaverMap 레이아웃 및 맵 로딩 구현

--- app/src/main/java/com/preonboarding/locationhistory/presentation/MainActivity.kt
+++ app/src/main/java/com/preonboarding/locationhistory/presentation/MainActivity.kt
@@ -2,11 +2,53 @@ package com.preonboarding.locationhistory.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 import com.preonboarding.locationhistory.R
+import com.preonboarding.locationhistory.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mapFragment: MapFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initMap()
+        bindViews()
+    }
+
+    private fun initMap() {
+        mapFragment = supportFragmentManager.findFragmentById(R.id.naverMapFragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.naverMapFragment, it).commit()
+            }
+
+        mapFragment.getMapAsync(this)
+    }
+
+
+    private fun bindViews() = with(binding) {
+        addressButton.setOnClickListener {
+            // TODO
+        }
+
+        historyButton.setOnClickListener {
+            // TODO
+        }
+
+        settingsButton.setOnClickListener {
+            // TODO
+        }
+    }
+
+
+    override fun onMapReady(naverMap: NaverMap) {
+
     }
 }
