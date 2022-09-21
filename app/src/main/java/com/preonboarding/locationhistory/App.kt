2168c4a61a6cf924293d792cf5f80fa32c165c97
Hyunkuk <boris0815@naver.com>
2022-09-21 17:19:49 +0900

    [feature] register worker manager

--- app/src/main/java/com/preonboarding/locationhistory/App.kt
+++ app/src/main/java/com/preonboarding/locationhistory/App.kt
@@ -1,11 +1,30 @@
 package com.preonboarding.locationhistory
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import javax.inject.Inject
 
 /**
  * @Created by 김현국 2022/09/19
  */
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder().setWorkerFactory(
+            workerFactory
+        ).build()
+}
