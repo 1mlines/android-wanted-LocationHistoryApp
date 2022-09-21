a3b3afe388436b39ea6f7b256d4a31481c5f944d
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-09-21 18:33:07 +0900

    Feature 히스토리 다이얼로그 구현
    xml은 임시로 구현하였습니다.

---
+++ app/src/main/java/com/preonboarding/locationhistory/view/HistoryDialog.kt
@@ -0,0 +1,65 @@
+package com.preonboarding.locationhistory.view
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.preonboarding.locationhistory.R
+import com.preonboarding.locationhistory.databinding.DialogHistoryBinding
+import java.util.*
+
+class HistoryDialog : DialogFragment() {
+    private var _binding: DialogHistoryBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.dialog_history, container, false)
+        val view = binding.root
+        initView()
+        return view
+    }
+
+    private fun initView() {
+        binding.tvHistoryDate.setOnClickListener {
+            showDatePicker()
+        }
+    }
+
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                binding.tvHistoryDate.text = "${year}.${month + 1}.${dayOfMonth}"
+            }
+        DatePickerDialog(
+            requireActivity(),
+            dateSetListener,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding.unbind()
+        _binding = null
+    }
+}
