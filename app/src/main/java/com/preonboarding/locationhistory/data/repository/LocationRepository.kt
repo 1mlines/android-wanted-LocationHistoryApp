cbaf805536955118d0689856d9172e811525a6b0
Hyunkuk <boris0815@naver.com>
2022-09-22 02:46:03 +0900

    [fix] entity format 변경

--- app/src/main/java/com/preonboarding/locationhistory/data/repository/LocationRepository.kt
+++ app/src/main/java/com/preonboarding/locationhistory/data/repository/LocationRepository.kt
@@ -1,13 +1,11 @@
 package com.preonboarding.locationhistory.data.repository
 
 import com.preonboarding.locationhistory.data.model.asModel
-import com.preonboarding.locationhistory.data.model.formatDateToString
 import com.preonboarding.locationhistory.data.source.local.datasource.LocationDataSource
 import com.preonboarding.locationhistory.data.source.local.entity.LocationEntity
 import com.preonboarding.locationhistory.presentation.model.Location
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import java.util.Date
 import javax.inject.Inject
 
 /**
@@ -20,9 +18,9 @@ class LocationRepository @Inject constructor(
         return locationDataSource.saveLocation(location)
     }
 
-    fun getLocationsWithDate(date: Date): Flow<List<Location>> {
+    fun getLocationsWithDate(date: String): Flow<List<Location>> {
         return flow {
-            val locations = locationDataSource.getLocationsWithDate(date = formatDateToString(date = date))
+            val locations = locationDataSource.getLocationsWithDate(date = date)
             if (locations.isNotEmpty()) {
                 emit(
                     locations.map { entity ->
