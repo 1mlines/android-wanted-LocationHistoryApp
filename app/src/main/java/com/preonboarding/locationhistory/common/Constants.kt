864804b2c0bc5743610e8c6110c202f853e5234c
JaesungLeee <viki0114@naver.com>
2022-09-22 02:05:44 +0900

    [FEAT] #7 사용자 현재 위치 받아오기 및 마커 찍기

--- app/src/main/java/com/preonboarding/locationhistory/common/Constants.kt
+++ app/src/main/java/com/preonboarding/locationhistory/common/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
 
     const val NAVER_CLIENT_ID: String = "inrrgd4o6g"
     const val NAVER_CLIENT_SECRET: String = "hqaWnBxBeOExrWr6lRRXGqjpvrcL7IxijtL4Jkj8"
+
+    const val LOCATION_PERMISSION_REQUEST_CODE = 10
 }
