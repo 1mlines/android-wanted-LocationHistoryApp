aec8926b2a82777abdb1d0f39e0156386e6c4940
gyurim <gyurim1250@gmail.com>
2022-09-21 20:19:12 +0900

    fix: private 변수 변경

--- app/src/main/java/com/preonboarding/locationhistory/presentation/ui/util/DateUtil.kt
+++ app/src/main/java/com/preonboarding/locationhistory/presentation/ui/util/DateUtil.kt
@@ -3,9 +3,9 @@ package com.preonboarding.locationhistory.presentation.ui.util
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-val dateFormatter = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA)
 val currentDate: String = System.currentTimeMillis().convertTimeStampToDate()
 val currentDateData: Long = convertDateToTimeStamp(currentDate)
 
+private val dateFormatter = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA)
 private fun Long.convertTimeStampToDate(): String = dateFormatter.format(this)
 private fun convertDateToTimeStamp(date: String): Long = dateFormatter.parse(date).time
