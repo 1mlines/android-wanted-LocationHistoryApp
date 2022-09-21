a71a6a5e7b8461b79f0fb544771746544280762e
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-09-21 14:36:57 +0900

    Fix 시간포맷 확장함수로 분리

--- app/src/main/java/com/preonboarding/locationhistory/data/entity/MapEntity.kt
+++ app/src/main/java/com/preonboarding/locationhistory/data/entity/MapEntity.kt
@@ -5,7 +5,6 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.text.SimpleDateFormat
-import java.util.*
 
 @Parcelize
 @Entity
@@ -14,9 +13,9 @@ data class History(
     val date: String,
     val latitude: Double,
     val longitude: Double,
-) : Parcelable {
-    fun getDate(date: Date): String {
-        val dateFormat = SimpleDateFormat("yyyy.MM.dd")
-        return dateFormat.format(date)
-    }
-}
+) : Parcelable
+
+fun Long.toFormatTime(date: Long): String = SimpleDateFormat("yyyy.MM.dd HH:mm").format(date)
+
+fun Long.toFormatDate(date: Long): String = SimpleDateFormat("yyyy.MM.dd").format(date)
+
