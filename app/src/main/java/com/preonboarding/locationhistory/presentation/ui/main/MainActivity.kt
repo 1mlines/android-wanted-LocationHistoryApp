5e8a93b44461df80eb3158eb2d47d23ea3fb6bac
tjrkdgnl <skh0818@hanyang.ac.kr>
2022-09-21 15:21:45 +0900

    refactor: BaseActivity를 상속받는 형태로 리팩토링 진행

--- app/src/main/java/com/preonboarding/locationhistory/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/preonboarding/locationhistory/presentation/ui/main/MainActivity.kt
@@ -1,24 +1,18 @@
 package com.preonboarding.locationhistory.presentation.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.databinding.DataBindingUtil
 import com.preonboarding.locationhistory.R
 import com.preonboarding.locationhistory.databinding.ActivityMainBinding
+import com.preonboarding.locationhistory.presentation.ui.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        binding.lifecycleOwner = this
         binding.viewModel = viewModel
     }
 }
