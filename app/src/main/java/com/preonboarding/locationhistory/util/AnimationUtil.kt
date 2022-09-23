5904d7348102a80a6e45cb5851958bf0af2b1f00
yeongjin <dudwls901@naver.com>
2022-09-23 13:30:20 +0900

    [CHORE] #3 주석 제거 - 0923

--- app/src/main/java/com/preonboarding/locationhistory/util/AnimationUtil.kt
+++ app/src/main/java/com/preonboarding/locationhistory/util/AnimationUtil.kt
@@ -6,6 +6,5 @@ import android.view.animation.AnimationUtils
 import com.preonboarding.locationhistory.R
 
 object AnimationUtil {
-    //내용 입력 안 하면 흔들흔들~
     fun shakeAnimation(context: Context): Animation = AnimationUtils.loadAnimation(context, R.anim.shake)
 }
