1adef29ba6518341a0be3a20709ad550d527f039
tjrkdgnl <skh0818@hanyang.ac.kr>
2022-09-23 15:41:54 +0900

    fix: interfaceModule scope 지정

--- app/src/main/java/com/preonboarding/locationhistory/data/di/InterfaceModule.kt
+++ app/src/main/java/com/preonboarding/locationhistory/data/di/InterfaceModule.kt
@@ -8,23 +8,27 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
 
 @Module
 @InstallIn(ActivityComponent::class)
 object InterfaceModule {
 
     @Provides
+    @ActivityScoped
      fun bindLoadUrlCallback(
         activity: Activity
     ): LoadUrlCallbackInterface? = activity as? LoadUrlCallbackInterface
 
 
     @Provides
+    @ActivityScoped
      fun bindLocationCallback(
         activity: Activity
     ): LocationCallbackInterface? = activity as? LocationCallbackInterface
 
     @Provides
+    @ActivityScoped
      fun bindShowMsgCallback(
         activity: Activity
     ): ShowMessageCallbackInterface? = activity as? ShowMessageCallbackInterface
