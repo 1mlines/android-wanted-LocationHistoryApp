990659b9872e0ca1c76a1eb2f66f25d276e9408b
leehandsub <gustjq7027@naver.com>
2022-09-23 16:49:59 +0900

    fix 시간설정 ui 변경

--- app/src/main/java/com/preonboarding/locationhistory/feature/set/SetTimeDialog.kt
+++ app/src/main/java/com/preonboarding/locationhistory/feature/set/SetTimeDialog.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.preonboarding.locationhistory.LocationHistoryApp
+import com.preonboarding.locationhistory.R
 import com.preonboarding.locationhistory.databinding.DialogSetTimeBinding
 
 class SetTimeDialog : DialogFragment() {
@@ -27,9 +29,17 @@ class SetTimeDialog : DialogFragment() {
                 dismiss()
             }
             btnSetTimePositive.setOnClickListener {
-                LocationHistoryApp.prefs.setTime =
-                    (editSetText.text.toString().toInt() * 60000L).toString()
-                dismiss()
+                if (editSetText.text.toString() == "" || editSetText.text.toString() == "0") {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.set_time_dialog_warning),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    LocationHistoryApp.prefs.setTime =
+                        (editSetText.text.toString().toInt() * 60000L).toString()
+                    dismiss()
+                }
             }
         }
     }
