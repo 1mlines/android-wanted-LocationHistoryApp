fb1343173967c8584589bc29d7ad750b52444f59
SeongHo Park <42669772+psh2849@users.noreply.github.com>
2022-09-23 11:21:31 +0900

    fix: companion object에서 object class로 변환

--- app/src/main/java/com/preonboarding/locationhistory/adapter/HistoryDialogBindingAdapter.kt
+++ app/src/main/java/com/preonboarding/locationhistory/adapter/HistoryDialogBindingAdapter.kt
@@ -3,31 +3,28 @@ package com.preonboarding.locationhistory.adapter
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
-class HistoryDialogBindingAdapter {
-    companion object {
+object HistoryDialogBindingAdapter {
+    @BindingAdapter("setNumber")
+    @JvmStatic
+    fun setNumber(textView: TextView, id: Long) {
+        textView.text = id.toString()
+    }
 
-        @BindingAdapter("setNumber")
-        @JvmStatic
-        fun setNumber(textView: TextView, id: Long) {
-            textView.text = id.toString()
-        }
+    @BindingAdapter("setLatitude")
+    @JvmStatic
+    fun setLatitude(textView: TextView, latitude: Double) {
+        textView.text = latitude.toString()
+    }
 
-        @BindingAdapter("setLatitude")
-        @JvmStatic
-        fun setLatitude(textView: TextView, latitude: Double) {
-            textView.text = latitude.toString()
-        }
+    @BindingAdapter("setLongitude")
+    @JvmStatic
+    fun setLongitude(textView: TextView, longitude: Double) {
+        textView.text = longitude.toString()
+    }
 
-        @BindingAdapter("setLongitude")
-        @JvmStatic
-        fun setLongitude(textView: TextView, longitude: Double) {
-            textView.text = longitude.toString()
-        }
-
-        @BindingAdapter("setDate")
-        @JvmStatic
-        fun setDate(textView: TextView, date: String) {
-            textView.text = date
-        }
+    @BindingAdapter("setDate")
+    @JvmStatic
+    fun setDate(textView: TextView, date: String) {
+        textView.text = date
     }
 }
